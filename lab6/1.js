f0c76793c06bdfa2442db14648a716e7842cbf42
pshandy <nikonovmd12@mail.ru>
2022-09-06 12:19:08 +0300

    lab6

--- lab6/1.js
+++ lab6/1.js
@@ -15,25 +15,26 @@
 
 */
 
-// {source_cathalog} {source_file} {result_cathalog} {result_file} {file_output}
-
-// cscript 3.js C:\Users\User\Documents\cscript\lab5\tmp before.txt C:\Users\User\Documents\cscript\lab5\after after.txt t.txt
-
 var source_cathalog = WScript.Arguments(0);
 var source_file = WScript.Arguments(1);
 var result_cathalog = WScript.Arguments(2);
 var result_file = WScript.Arguments(3);
 
 var Shell = WScript.CreateObject("WScript.Shell");
+var fso = WScript.CreateObject("Scripting.FileSystemObject");
+
+// {source_cathalog} {source_file} {result_cathalog} {result_file} {file_output}
+// cscript 1.js C:\Users\User\Documents\cscript\lab6\tmp before.txt C:\Users\User\Documents\cscript\lab6\after after.txt output.txt
 
 // 6.1 Создать исходный каталог;
-Shell.Run('%COMSPEC% /C mkdir ' + source_cathalog );
+fso.CreateFolder(source_cathalog);
 // 6.2 С помощью Блокнота создать исходный файл;
+fso.CreateTextFile(source_cathalog +"\\" + source_file, true);
 Shell.Run(('notepad.exe "' + source_cathalog + '\\' + source_file + '"'), 1, true);
 // 6.3 Переместить исходный каталог в результирующий каталог;
-Shell.Run('%COMSPEC% /C move ' + source_cathalog  + ' ' + result_cathalog);
+fso.MoveFolder(source_cathalog, result_cathalog);
 // 6.4 Переименовать файл в результирующем каталоге;
-Shell.Run('%COMSPEC% /C rename "' + result_cathalog + '\\' + source_file + '" "' + result_file + '"');
+fso.GetFile(result_cathalog + "\\" + source_file).Name = result_file;
 // 6.5 Запустить Блокнот для редактирования файла в результирующем каталоге;
 Shell.Run(Shell.ExpandEnvironmentStrings('"%WINDIR%\\notepad.exe" "' + result_cathalog +'\\'+ result_file + '"'), 1, true);
 // 6.6 Вывести оглавление результирующего каталога;
