9566a05cb45f53f50a00a4884704178590c70b4e
pshandy <nikonovmd12@mail.ru>
2022-09-06 21:23:18 +0300

    doc_lab4

--- lab5/1.js
+++ lab5/1.js
@@ -5,7 +5,7 @@
 параметров командной строки при вызове процедуры.
 */
 
-//cscript 1.js md {path} {filename}
+//cscript 1.js {command} {path} {filename}
 //cscript 1.js md C:\Users\User\Documents\cscript\lab5\ out.txt
 
 var shell = WScript.CreateObject("WScript.Shell");
@@ -19,5 +19,5 @@ if (WScript.Arguments.count() == 3) {
 	var command = WScript.Arguments(0);
 	shell.Run("%COMSPEC% /K help " + command);
 } else {
-	shell.Run("%COMSPEC% /K help ");
+	shell.Run("%COMSPEC% /K help");
 }
